2136fc5b5ac169398d4e805cfa999dfe6f1c0dec
Walid Sultan <walidalysultan@gmail.com>
2015-04-27 01:39:37 +0200

    Wrong file name check

--- www/js/Game/Modules/LevelLoader.js
+++ www/js/Game/Modules/LevelLoader.js
@@ -33,7 +33,7 @@
         },
         getLevelPath: function () {
             if (this.levelIndex <= 3) {
-                return 'Levels/Easy/level' + this.levelIndex + '.xml';
+                return 'Levels/Easy/levhkjhkjhel' + this.levelIndex + '.xml';
             }
             else if (this.levelIndex <= 11) {
                 return 'Levels/SokobanJunior/level' + (this.levelIndex - 3) + '.xml';
