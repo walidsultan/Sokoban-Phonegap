41cd2ca87ed7fbfd0e56e8b4bb9349a793566e71
Walid Sultan <walidalysultan@gmail.com>
2015-04-27 02:48:09 +0200

    Added debugger variables

--- www/js/Game/Modules/LevelLoader.js
+++ www/js/Game/Modules/LevelLoader.js
@@ -15,11 +15,14 @@
         loadXMLDoc: function (levelPath) {
             var me = this;
 			$.get(levelPath, function (data) {
+				alert(data);
 			    var xmlDoc = $(data);
-                me.levelRows = xmlDoc.find("L");
+                                alert(xmlDoc);
+				me.levelRows = xmlDoc.find("L");
                 me.levelRendered = false;
                 var levelIdentifier = xmlDoc.find("Level");
                 var levelWidth = levelIdentifier[0].getAttribute('Width');
+				                alert(levelWidth);
                 var levelHeight = levelIdentifier[0].getAttribute('Height');
                 var levelId = levelIdentifier[0].getAttribute('Id');
                 $(window).trigger('updateLevelName', levelId);
