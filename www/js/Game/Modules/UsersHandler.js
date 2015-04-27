a33a6d725625f5ce813b24075a987e0790061fd3
Walid Sultan <walidalysultan@gmail.com>
2015-04-28 00:24:28 +0200

    Added localstorage instead of cookies

--- www/js/Game/Modules/UsersHandler.js
+++ www/js/Game/Modules/UsersHandler.js
@@ -1,46 +1,30 @@
 ﻿(function (ns) {
     ns.UsersHandler = skui.extend(function () {
         this.levelIndex = 1;
-        var userCookie = this.getCookie('sokobanUser');
-        if (userCookie != '') {
-            userCookie = JSON.parse(userCookie);
-            this.levelIndex = userCookie.levelIndex;
+        var sokobanUser = localStorage.getItem('sokobanUser');
+        if (sokobanUser != null) {
+            sokobanUser = JSON.parse(sokobanUser);
+            this.levelIndex = sokobanUser.levelIndex;
         } else {
             var sokobanUser = {
                 userId: guid(),
                 levelIndex: 1
             };
-            this.setCookie('sokobanUser', JSON.stringify(sokobanUser), 365);
+            localStorage.setItem('sokobanUser', JSON.stringify(sokobanUser));
         }
-       // $(window).on('levelSolved', this.saveUserLevelIndex.bind(this));
+        $(window).on('levelSolved', this.saveUserLevelIndex.bind(this));
         $(window).on('setLevelIndex', this.setCurrentLevelIndex.bind(this));
 
         $(window).trigger('loadMenu', { levelIndex: this.levelIndex });
     }, {
-        setCookie: function (cname, cvalue, exdays) {
-            var d = new Date();
-            d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-            var expires = "expires=" + d.toUTCString();
-            document.cookie = cname + "=" + cvalue + "; " + expires;
-        },
-        getCookie: function (cname) {
-            var name = cname + "=";
-            var ca = document.cookie.split(';');
-            for (var i = 0; i < ca.length; i++) {
-                var c = ca[i];
-                while (c.charAt(0) == ' ') c = c.substring(1);
-                if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
-            }
-            return "";
-        },
         saveUserLevelIndex: function () {
             this.currentLevelIndex++;
             if (this.currentLevelIndex > this.levelIndex) {
-                var userCookie = JSON.parse(this.getCookie('sokobanUser'));
+                var sokobanUser = JSON.parse(localStorage.getItem('sokobanUser'));
                 this.levelIndex = this.currentLevelIndex;
-                if (userCookie != null) {
-                    userCookie.levelIndex = this.levelIndex;
-                    this.setCookie('sokobanUser', JSON.stringify(userCookie), 365);
+                if (sokobanUser != null) {
+                    sokobanUser.levelIndex = this.levelIndex;
+                    localStorage.setItem('sokobanUser', JSON.stringify(sokobanUser));
                 }
             }
         },
